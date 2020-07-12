465fb16a387d1ae787a756f00b7c9895cbe0ae4a
marcodca <marcodecara@gmail.com>
2020-07-12 19:34:39 +0200

    env

--- functions/helpers/send-query.js
+++ functions/helpers/send-query.js
@@ -1,12 +1,13 @@
 const axios = require("axios");
+const dotenv = require("dotenv").config()
 
 module.exports = async (query, variables) => {
   const result = await axios({
     url: "https://marcodca.herokuapp.com/v1/graphql",
     method: "POST",
     headers: {
-        "Content-type": "application/json"
-    //   Authorization: `Bearer ${process.env.FAUNA_SERVER_SECRET}`
+        "Content-type": "application/json",
+        "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET
     },
     data: {
       query,
